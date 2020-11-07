642ab2d21391fb1ca0279bcad6ab9d7e6270eee9
Rafael Ortiz <rortiz@previred.com>
2020-11-06 23:47:11 -0300

    Se agrega funcionalidad de resta y sus test

--- calculadora10.py
+++ calculadora10.py
@@ -19,3 +19,25 @@ def sumaTest():
         print("test correcto -1+1=0")
     else:
         print("test incorrecto -1+1=0 resultado "+resultado)
+
+def resta(a, b):
+    return a - b
+
+def restaTest():
+    resultado = resta(1, 1)
+    if(int(resultado) == 0):
+        print("test correcto 1-1=0")
+    else:
+        print("test incorrecto 1-1=0 resultado "+str(resultado))
+
+    resultado = resta(3, 2)
+    if(int(resultado) == 1):
+        print("test correcto 3-2=1")
+    else:
+        print("test incorrecto 3-2=1 resultado "+str(resultado))
+
+    resultado = resta(-1, 1)
+    if(int(resultado) == -2):
+        print("test correcto -1-1=-2")
+    else:
+        print("test incorrecto -1-1=-2 resultado "+str(resultado))
