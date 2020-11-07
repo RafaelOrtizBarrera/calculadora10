7e31a9c2a710a61088565c4df48c1015069ebe69
Rafael Ortiz <rortiz@previred.com>
2020-11-06 23:51:56 -0300

    Se agrega funcion multiplicar y sus test

--- calculadora10.py
+++ calculadora10.py
@@ -65,3 +65,25 @@ def divisionTest():
         print("test correcto 8/4=2")
     else:
         print("test incorrecto 8/4=2 resultado "+str(resultado))
+
+def multiplicacion(a, b):
+    return a * b
+
+def multiplicacionTest():
+    resultado = multiplicacion(1, 1)
+    if(int(resultado) == 1):
+        print("test correcto 1*1=1")
+    else:
+        print("test incorrecto 1*1=1 resultado "+str(resultado))
+
+    resultado = multiplicacion(2, 2)
+    if(int(resultado) == 4):
+        print("test correcto 2*2=4")
+    else:
+        print("test incorrecto 2*2=4 resultado "+str(resultado))
+
+    resultado = multiplicacion(2, 0)
+    if(int(resultado) == 0):
+        print("test correcto 2*0=0")
+    else:
+        print("test incorrecto 2*0=0 resultado "+str(resultado))
